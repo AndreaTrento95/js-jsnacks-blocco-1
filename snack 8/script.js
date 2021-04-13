120a006903c0510c71ba6cf87fc12b90e07f1e64
AndreaTrento95 <trentora95@gmail.com>
2021-04-13 16:04:31 +0200

    svolgimento

--- snack 8/script.js
+++ snack 8/script.js
@@ -1,8 +1,16 @@
 // ESERCIZIO: Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
 
 // Chiedo all'utente di inserire un numero a quattro cifre
-var num_utente = parseInt(prompt("Inserisci un numero a quattro cifre"));
+var num_utente = (prompt("Inserisci un numero a quattro cifre"));
 
-// 
+var somma = 0;
+
+for(var i = 0; i < num_utente.length; i++){
+    var numero = parseInt(num_utente[i]);
+    somma = somma + numero;
+}
+
+// output
+console.log(somma);
 
 
