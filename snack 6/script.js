c3f64a77322ee20c622ed5042d522cdc8a525b97
AndreaTrento95 <trentora95@gmail.com>
2021-04-13 12:50:39 +0200

    svolgimento

--- snack 6/script.js
+++ snack 6/script.js
@@ -9,6 +9,7 @@ var counterPotenza = 1; // contatore delle potenze
 
 while(potenza < limite){
     console.log(potenza);
+    console.log("elevo il 2 alla"+counterPotenza);
     potenza = Math.pow(2, counterPotenza);
     counterpotenza++;
 }
@@ -16,7 +17,12 @@ while(potenza < limite){
 
 
 
-
-
-
 // SOLUZIONE 2
+var limite = 1000;
+var potenza = 1;
+var counterPotenza = 1; // contatore delle potenze
+
+while(potenza < limite){
+    console.log(potenza);
+    potenza *=2;
+}
